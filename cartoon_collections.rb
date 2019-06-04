a2b4c85e934f072f92cee8329f4bf8d740317162
Gilda Snow <gilda.snow@gmail.com>
2019-06-04 23:24:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,6 @@ def long_planeteer_calls(planeteer_calls)
 end
 
 def find_the_cheese(snacks)
-  snacks.find do |cheeses|
+  snacks.find do |cheese|
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
